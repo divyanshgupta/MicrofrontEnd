01c2b3efbfb49d4fd1e26ea14ad3420163539dd5
divyansh gupta <divyansh.gupta@iongroup.com>
2019-12-10 16:04:23 +0530

    Pushing to npm registry Creating production builds with specific versions of packages.

---
+++ packages/ms-books/index.js
@@ -0,0 +1,9 @@
+import { Books } from './src/components/Books';
+import BookImage from './src/images/books.jpg';
+
+export const setupConfig = {
+  url: '/books',
+  Component: Books,
+  image: BookImage,
+  title: 'Books',
+}
---
+++ packages/ms-cosmetics/index.js
@@ -0,0 +1,9 @@
+import { Cosmetics } from './src/components/Cosmetics';
+import CosmeticsImage from './src/images/cosmetics.jpg';
+
+export const setupConfig = {
+  url: '/cosmetics',
+  Component: Cosmetics,
+  image: CosmeticsImage,
+  title: 'Cosmetics',
+}
